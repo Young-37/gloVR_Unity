33ad3b0553d7e757e55a93ca61113d2fe37927cd
qkrwjdan <madogisa12@naver.com>
2020-09-14 12:46:56 +0900

    Menu 만들고 SceneManager로 장면 전환

---
+++ gloVR/Assets/Scripts/btnType.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class btnType : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public BTNType currentType;
+    public Transform buttonScale;
+    Vector3 defaultScale;
+
+    private void Start(){
+        defaultScale = buttonScale.localScale;
+    }
+
+    public void OnBtnClick(){
+
+        switch(currentType){
+            case BTNType.Start:
+            SceneManager.LoadScene("HandMotion");
+            break;
+
+            case BTNType.Setting:
+            Debug.Log("Setting");
+            break;
+
+            case BTNType.Exit:
+            Application.Quit();
+            Debug.Log("Exit!");
+            break;
+        }
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData){
+        buttonScale.localScale = defaultScale * 1.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+        buttonScale.localScale = defaultScale;
+    }
+}
