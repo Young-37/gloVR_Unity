dc08ce1d782a06c4542ced0ac6f1490447289fbb
1996yyk <1996yyk@gmail.com>
2020-09-23 18:27:14 +0900

    Send to Python
    .

--- gloVR/Assets/Scripts/MainUI.cs
+++ gloVR/Assets/Scripts/MainUI.cs
@@ -15,12 +15,12 @@ public class MainUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ gloVR/Assets/Scripts/cToPy.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDP
+{
+  public class cToPy : MonoBehaviour
+  {
+      // Start is called before the first frame update
+      public void SendHello()
+      {
+        UDPSocket s = new UDPSocket();
+        s.Server("127.0.0.1",27000);
+        s.Send("Hello World!");
+
+        Console.ReadKey();
+      }
+
+      // Update is called once per frame
+      void Update()
+      {
+
+      }
+  }
+}
