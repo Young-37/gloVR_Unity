06bac10d820b59e90ba50941d50514fe36820a09
1996yyk <1996yyk@gmail.com>
2020-09-26 13:08:10 +0900

    여러버튼

--- gloVR/Assets/Scripts/cToPy.cs
+++ gloVR/Assets/Scripts/cToPy.cs
@@ -10,15 +10,35 @@ namespace UDP
 {
   public class cToPy : MonoBehaviour
   {
-      // Start is called before the first frame update
-      public void SendHello()
+      UDPSocket c = new UDPSocket();
+
+
+      public void SendInGame()
       {
+        c.Client("127.0.0.1", 8000);
         /*UDPSocket s = new UDPSocket();
         s.Server("127.0.0.1",27000);
         s.Send("Hello World!");*/
-        UDPSocket c = new UDPSocket();
+        c.Send("s");
+
+        Console.ReadKey();
+      }
+
+      public void SendOutGame()
+      {
         c.Client("127.0.0.1", 8000);
-        c.Send("a");
+        c.Send("1");
+
+        Console.ReadKey();
+      }
+
+      public void SendEnd()
+      {
+        c.Client("127.0.0.1", 8000);
+        /*UDPSocket s = new UDPSocket();
+        s.Server("127.0.0.1",27000);
+        s.Send("Hello World!");*/
+        c.Send("e");
 
         Console.ReadKey();
       }
